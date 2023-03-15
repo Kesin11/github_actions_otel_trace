670d1264bed7116bcf71419fa905ef93c485a58e
Kenta Kase <kesin1202000@gmail.com>
2023-03-15 02:33:53 +0000

    Fix log and commentout

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 COLLECTOR_CONFIG_YAML=${COLLECTOR_CONFIG_YAML:-./otel-collector-config.yaml}
-echo $COLLECTOR_CONFIG_YAML
+echo "COLLECTOR_CONFIG_YAML=${COLLECTOR_CONFIG_YAML}"
 
 ./otelcol-contrib --config "${COLLECTOR_CONFIG_YAML}" & npm run start
